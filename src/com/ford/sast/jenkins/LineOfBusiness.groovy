40bdfcab43ee1e9237c59a21d114725c8f1bd02a
Randy Geyer <randy@checkmarx.com>
2019-03-15 11:20:06 -0500

    Work on LoB enum

--- src/com/ford/sast/jenkins/LineOfBusiness.groovy
+++ src/com/ford/sast/jenkins/LineOfBusiness.groovy
@@ -1,24 +1,37 @@
 package com.ford.sast.jenkins
 
-enum LineOfBusiness {
+class LineOfBusiness {
 
-    // TODO: replace names with proper full names
-    AV ('AV'),
-    CV('CV'),
-    EV('EV'),
-    FordCredit('FordCredit'),
-    FordPass('FordPass'),
-    FoundationalServices('FoundationalServices'),
-    FSM('FSM'),
-    GDIA('GDIA'),
-    IT('IT')
-
-    LineOfBusiness(String fullname) {
-        this.fullname = fullname
+    private enum LoB {
+        // TODO: replace names with proper full names
+        AV ('AV'),
+        CV('CV'),
+        EV('EV'),
+        FordCredit('FordCredit'),
+        FordPass('FordPass'),
+        FoundationalServices('FoundationalServices'),
+        FSM('FSM'),
+        GDIA('GDIA'),
+        IT('IT')
+    
+        LoB(String fullname) {
+            this.fullname = fullname
+        }
+    
+        private final String fullname
+        String getFullname() {
+            fullname
+        }
     }
-
-    private final String fullname
-    String getFullname() {
-        fullname
-    }
+    
+    static final String AV = LoB.AV
+    static final String CV = LoB.CV
+    static final String EV = LoB.EV
+    static final String FordCredit = LoB.FordCredit
+    static final String FordPass = LoB.FordPass
+    static final String FoundationalServices = LoB.FoundationalServices
+    static final String FSM = LoB.FSM
+    static final String GDIA = LoB.GDIA
+    static final String IT = LoB.IT
+    
 }
